5ddcf785d7795e8a2c054825efe79592b2b1281a
amauget <amauget@hotmail.com>
2024-11-22 12:32:21 -0800

    Test Product.jsx and Refactor Props ProductRender to test all components ProductEvents to test data passage w/ events

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,12 +1,12 @@
 import './Product.css'
 import { Link } from 'react-router-dom'
 
-export default function Product({ image, title, rating, price, handleAddToCart, handleViewItem }){
+export default function Product({ image, title, rate, count, price, handleAddToCart, handleViewItem }){
     return(
         <div className="productContainer">  
             <Link to='/ProductInfoPage'><img className="productImg" src={image} onClick={handleViewItem} /></Link>
             <Link to='/ProductInfoPage' className="title" onClick={handleViewItem}>{title}</Link>
-            <h3 className="rating">{rating.rate} Stars ({rating.count} Reviews)</h3>
+            <h3 className="rating">{rate} Stars ({count} Reviews)</h3>
             <h3 className="price">${price}</h3>
             <button onClick={handleAddToCart} className="addToCart">Add To Cart</button>
         </div>
